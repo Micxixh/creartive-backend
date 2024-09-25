d55be779aa7f9956687d37a94edf295a0db40bf1
Micxixh <micaiah.viaverso@gmail.com>
2024-09-25 12:25:53 +0100

    login

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import userSchema from './user'
+import pinSchema from './pin'
+import commentSchema from './comment'
+import saveSchema from './save'
+import postedBy from './postedBy'
+
+
+export const schemaTypes = [
+    userSchema,
+    pinSchema,
+    commentSchema,
+    saveSchema,
+    postedBy
+
+]
---
+++ schemaTypes/user.js
@@ -0,0 +1,27 @@
+export default {
+    name: 'user',
+    title: 'User',  
+    type: 'document',
+    fields: [    
+        {
+          name: 'userName',      
+          title: 'UserName',      
+          type: 'string',     
+        },    
+        {      
+          name: 'image',      
+          title: 'Image',      
+          type: 'string',  
+        },
+        {
+          name: 'password',
+          title: 'Password',
+          type: 'string',
+        },
+        {
+          name: 'Email',
+          title: 'Email',
+          type: 'string',
+        },
+    ],
+}
